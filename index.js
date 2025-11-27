e49f66bdfa6970ee1c51082335732abc3cae28e5
Igor123qwe <2321158@gmail.com>
2025-11-27 15:12:27 +0300

    update backend

--- index.js
+++
@@ -1,8 +0,0 @@
-// index.js — простой entrypoint для Vercel
-
-export default function handler(req, res) {
-  res.status(200).json({
-    ok: true,
-    message: 'ProGid backend is running',
-  })
-}
