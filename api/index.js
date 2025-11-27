6b265a1e4821e2abf96b91af320a969bce5bd0a2
Igor123qwe <2321158@gmail.com>
2025-11-27 15:24:30 +0300

    update backend

--- api/index.js
+++ api/index.js
@@ -1,14 +1,6 @@
-// api/index.js
-// Главный endpoint: GET /api
-
 export default function handler(req, res) {
-  if (req.method !== 'GET') {
-    res.setHeader('Allow', 'GET');
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-
-  return res.status(200).json({
+  res.status(200).json({
     ok: true,
-    message: 'Progid backend is running (serverless /api)',
+    message: "Progid backend serverless is running"
   });
 }
